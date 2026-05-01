675d8217ae96ab7daf03a2e4a3ca10d4366eaaf7
fabio henrique <fabiohosc@gmail.com>
2026-04-30 21:00:54 -0300

    Tentendo corrigir as coisas, parte 2

--- principal.js
+++ principal.js
@@ -4,7 +4,7 @@ var bannerAtual = 0;
 
 function trocaBanner() {
     bannerAtual = (bannerAtual + 1) % 2; 
-    document.querySelector('h2#mensagem').textContent = banners[bannerAtual];
+    document.querySelector("h2#mensagem").textContent = banners[bannerAtual];
 }
 
 setInterval(trocarBanner, 1000);
