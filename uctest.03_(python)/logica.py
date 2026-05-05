1137306995c37ee2b50dd4ec0b816399c018b8be
fabio henrique <fabiohosc@gmail.com>
2026-05-05 19:50:23 -0300

    Adicionando o teste

---
+++ uctest.03_(python)/logica.py
@@ -0,0 +1,19 @@
+import pytest
+from uctest.02_(python) import uctest.01.py, uctest.02.py, uctest.py
+
+@pytest.fixture9(autouse=True)
+def reset_usuarios():
+    usuarios.clear()
+    usuarios.update({"admin": "1234"})
+
+def test_login_valido():
+    assert verificar_login("admin", "1234") == True
+
+def test_login_usuario_inexistente():
+    assert verificar_login("joao", "1234") == False
+
+def test_login_senha_errada():
+    assert verificar_login("admin", "0000") == False
+
+def test_login_campos_vazios():
+    assert verificar_login("", "") == False
